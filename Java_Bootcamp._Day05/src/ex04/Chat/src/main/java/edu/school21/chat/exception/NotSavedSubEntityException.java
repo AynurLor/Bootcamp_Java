d9a03533189aee559e3d1d28d329c282a9c821d2
Aynur Biktagoirov <biktagirovainur20@gmail.com>
2023-04-19 00:31:11 +0300

    Delete Java_Bootcamp._Day05 directory

--- Java_Bootcamp._Day05/src/ex04/Chat/src/main/java/edu/school21/chat/exception/NotSavedSubEntityException.java
+++
@@ -1,8 +0,0 @@
-package edu.school21.chat.exception;
-
-public class NotSavedSubEntityException extends RuntimeException{
-  @Override
-  public void printStackTrace() {
-    System.err.println("Cannot save message");
-  }
-}

4ed0dd6c11d0b9cef01f7304237b40c2d7a9e1a5
Aynur Biktagoirov <biktagirovainur20@gmail.com>
2023-04-19 00:32:01 +0300

    Delete Java_Bootcamp._Day09 directory

--- Java_Bootcamp._Day09/src/ex01/Chat/Char/src/main/java/edu/school21/sockets/repositories/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.school21.sockets.repositories;
-
-import java.util.Optional;
-
-public interface UsersRepository<T> extends CrudRepository{
-    Optional<T> findByName(String name);
-}
-

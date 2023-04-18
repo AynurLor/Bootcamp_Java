bfcac5a60afb90e7b0ef071796138c1131cac1ee
Aynur Biktagoirov <biktagirovainur20@gmail.com>
2023-04-19 00:31:23 +0300

    Delete Java_Bootcamp._Day06 directory

--- Java_Bootcamp._Day06/src/ex01/Tests/src/test/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into product(name, price) values ('potato', 100);
-insert into product(name, price) values ('milk', 50);
-insert into product(name, price) values ('cheese', 300);
-insert into product(name, price) values ('tomato', 200);
-insert into product(name, price) values ('courage', 999);
-
-
